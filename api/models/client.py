7a5565c63373eb4bd641b7439abbd0fa788a7617
devproggithub <dev.prognft@gmail.com>
2025-04-26 17:26:39 +0100

    add the files

---
+++ api/models/client.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20))
+    address = db.Column(db.String(200))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            "address": self.address
+        }
